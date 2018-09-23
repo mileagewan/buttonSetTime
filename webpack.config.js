00478ef289389a10b85e6b23f2f117984c68455e
mileagewan <41629596+mileagewan@users.noreply.github.com>
2018-09-23 09:23:56 +0800

    123455

---
+++ webpack.config.js
@@ -0,0 +1,10 @@
+var path = require('path');
+
+module.exports = {
+  mode: 'development',
+  entry: './test.js',
+  output: {
+    path: path.resolve(__dirname, 'dist'),
+    filename: 'bundle.js'
+  }
+};
